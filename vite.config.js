716d94ac49f3047559939f43194162126af4cf57
chellapuramdurgaprasad <107021920+chellapuramdurgaprasad@users.noreply.github.com>
2025-11-22 07:06:25 +0530

    vite dist changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // <--- Change the output directory here
+    outDir: 'dist' // <--- Change the output directory here
   }
 })
